969badb6ba7b5735849d909ea412c74e1b10d816
PotatoSalad531 <datuna.ziraqishvili@gmail.com>
2024-05-09 01:29:48 +0400

    getting video input and grayscaling

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 
 using namespace cv;
 
+void matchAscii(gray) {
+
+}
+
 int main(int argc, char** argv) {
     std::cout << "harro :3\n";
 
@@ -14,13 +18,23 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    Mat frame { };
     while (1) {
-        if (!cap.read(frame)) {
-            break;
-        }
+        Mat frame;
+        cap >> frame;
 
+        if (frame.empty())
+            break;
+
+        Mat gray;
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+
+        imshow("demo", gray);
+
+        waitKey(0);
     }
+
+    cap.release();
+    destroyAllWindows();
 
     return 0;
 }
